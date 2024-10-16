a8b23aa93de75087042691187ca7abc1cf881b46
JUA <hjua7@naver.com>
2024-10-16 09:45:51 +0900

    five

--- src/f201732039/Main.java
+++ src/f201732039/Main.java
@@ -6,7 +6,8 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("홍주아");
 		World.run();
-		 Hello.run();
+		Hello.run();
+		World.run2();
 	}
 
 }
--- src/f201732039/World.java
+++ src/f201732039/World.java
@@ -4,4 +4,7 @@ public class World {
 	public static void run() {
 		System.out.println("201732039");
 	}
+	public static void run2() {
+	    System.out.println("hello world");
+	  }
 }
